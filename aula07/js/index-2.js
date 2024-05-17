4510d35c765c162e549ca677dbc7c737a6cd4a7e
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-05-17 14:54:58 -0300

    aulas

--- aula07/js/index-2.js
+++ aula07/js/index-2.js
@@ -1,12 +1,14 @@
 var a = document.getElementById('btnn');
 a.addEventListener('click', press);
 
+
 function press() {
-    var n = document.getElementById('txtbr');
+    var n = document.getElementById('txtbr').value;
     var x = document.getElementById('res');
-    if (n != 'Brazil') {
-        x.innerHTML = `You are a foreigner!`;
-    } else {
-        x.innerHTML = `Congrats! You"re from Brazil!`;
+    if (n == 'Brazil') {
+        x.innerHTML = `Congrats! You are from Brazil!`;
+    }
+     else {
+         x.innerHTML = `You are a foreigner!`;
     }
 }
