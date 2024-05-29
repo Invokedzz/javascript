3066d2b7043ea821e9597ffa631489957db0426b
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-05-29 20:58:04 -0300

    Create scripts.js

---
+++ array/scripts.js
@@ -0,0 +1,24 @@
+/*let num = [5, 8, 4]; // Det do array;
+num.sort(); // Ordem crescente do array;
+for (let pos = 0; pos < num.length; pos++) {
+    console.log(num[pos]);
+}/*/
+
+function loucura(x) {
+  var z =  x*x + 1;
+    return z
+}
+
+
+console.log(loucura(1))
+
+function parimpar(y) {
+    if (y % 2 == 0) {
+        return 'par';
+    } else {
+        return 'impar';
+    }
+}
+
+console.log(parimpar(4));
+
