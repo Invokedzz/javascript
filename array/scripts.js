6b925b93f6c0e8fefdf3e99d66b632e78b6d6831
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-05-31 14:03:43 -0300

    Update scripts.js

--- array/scripts.js
+++ array/scripts.js
@@ -19,6 +19,30 @@ function parimpar(y) {
         return 'impar';
     }
 }
-
+// String = texto, Number = numero;
 console.log(parimpar(4));
 
+let nomecliente = 'Osvaldo';
+console.log('Ele e', nomecliente ,'foram ao mercado');
+let heroiTerra = 'Xandão';
+console.log('Numa longa batalha,',heroiTerra,'obteve o seu lucro.');
+console.log('Mas,',heroiTerra, 'foi confrontado por',nomecliente);
+
+const primeiroNumero = 50;
+const segundoNumero = 60;
+let expressao = Number(primeiroNumero + segundoNumero)/2;
+const expressaoDuplicada = (expressao * 2);
+const expressaovezesSeis = Number(expressaoDuplicada * 6);
+console.log(expressaovezesSeis)
+
+let nomePaciente = 'Super Xand'; 
+let sobrenomePaciente = 'Ultimo Heroi da Terra';
+let idadePaciente = '2024 - 1990';
+let pesoPaciente = '100'
+let alturaPaciente = '1.8';
+let IMCPaciente = '25.925925';
+let imc = Number(pesoPaciente / (alturaPaciente * alturaPaciente));
+
+console.log(imc)
+
+console.log('Atendemos hoje', nomePaciente + sobrenomePaciente, 'ele possui' ,idadePaciente, 'mede' ,pesoPaciente, 'tem', alturaPaciente, 'e seu IMC eh igual a', IMCPaciente,)
