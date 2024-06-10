40ed2ada46ddc191336e959d1243fb8e2403d4b8
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-06-10 08:57:53 -0300

    Update scripts-3.js

--- exercises/scripts-3.js
+++ exercises/scripts-3.js
@@ -8,3 +8,17 @@ for (let j = 0; j < elementos.length; j++) {
     const {nome, sobrenome} = elementos[j];
     console.log(nome, sobrenome);
 }
+let livros = [ 'Bras Cubas', 'Crônicas de Gelo e Fogo', 'Príncipe dos Espinhos', 'Como eu era antes de você'];
+const novosLivros = livros.push('As trevas de Sethanon', 'Um curso de Cálculo', 'Diário de um Banana');
+let TodosLivros = [
+    {titulo: '', subtitulo: ''},
+    {titulo: '', subtitulo: ''},
+    {titulo: '', subtitulo: ''},
+    {titulo: '', subtitulo: ''},
+]
+/*for (let i = 0; i < livros.length; i++) {
+    const {titulo, subtitulo} = livros[i];
+    console.log(titulo, subtitulo);
+} */
+console.log(novosLivros);
+console.log(livros);
