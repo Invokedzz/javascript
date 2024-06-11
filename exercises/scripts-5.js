499bcf229902f068bbf75da8174864b03a41bd33
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-06-11 08:53:11 -0300

    data files

--- exercises/scripts-5.js
+++ exercises/scripts-5.js
@@ -103,4 +103,33 @@ try {
 } catch (err) {
     console.log('Amigável');
 }
- 
+try {
+    console.log('Abri um arquivo');
+    console.log('Manipulei o arquivo e gerou erro');
+    console.log('Fechei o arquivo');
+} catch (e) {
+    console.log('Tratando o erro');
+} finally {
+    console.log('Eu sempre sou executado');
+}
+function retornaHora(data) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instância de Date');
+    }
+    if (!data) {
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false
+    });
+}
+try {
+    const data = new Date('01-01-1970 12:58:12')
+    const hora = retornaHora();
+    console.log(hora);
+} catch (e) {
+    console.log(e)
+} finally {
+    console.log('Tenha um bom dia.')
+}
